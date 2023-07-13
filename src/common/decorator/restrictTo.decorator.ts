aacc4c3edc25e3c1aeb2059095a23f970dc8ffe6
Erfan <erfanmahvash12345@gmail.com>
2023-07-13 19:45:13 +0330

    feature: multi role access

--- src/common/decorator/restrictTo.decorator.ts
+++ src/common/decorator/restrictTo.decorator.ts
@@ -3,9 +3,9 @@ import { RestrictToKey } from 'src/constant/String';
 import { UserState } from 'types/user';
 import { RestrictToGuard } from '../guard/restrict-to.guard';
 
-export function RestrictTo(state: UserState) {
+export function RestrictTo(state: UserState | UserState[]) {
   return applyDecorators(
-    SetMetadata(RestrictToKey, state),
+    SetMetadata(RestrictToKey, Array.isArray(state) ? state : [state]),
     UseGuards(RestrictToGuard),
   );
 }
